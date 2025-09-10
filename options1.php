142fc6c70829ce4b730f98cdfa8bb98f78e17a4f
Keys <keys14052002@gmail.com>
2025-09-10 16:08:00 +0000

    feat: apply the color to the color me text

--- options1.php
+++ options1.php
@@ -1,6 +1,5 @@
 <?php
     header("Content-type: application/xml");
-    header("Access-Control-Allow-Origin: *");
     $options = ['red', 'blue', 'green']; 
     echo '<?xml version="1.0"?>';
     echo '<options>';
