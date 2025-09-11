3cfdbd94a1360888b7424dded2096e6bdcc80202
Keys <keys14052002@gmail.com>
2025-09-11 08:31:57 +0000

    chore: add info about the scheme in the option element

--- options1.php
+++ options1.php
@@ -1,6 +1,9 @@
 <?php
     header("Content-type: application/xml");
+
     $options = ['red', 'blue', 'green']; 
+
+    /*
     echo '<?xml version="1.0"?>';
     echo '<options>';
 
@@ -12,4 +15,23 @@
     }
 
     echo '</options>';
+    */
+
+    $template = <<<XML
+    <?xml version="1.0" encoding="UTF-8"?>
+    <options>
+    XML;
+
+    foreach ($options as $value) {
+        $template .= <<<XML
+        <option>$value</option>
+        XML;
+    }
+
+    $template .= <<<XML
+    </options>
+    XML;
+
+    echo $template;
 ?>
+
