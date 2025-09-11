9bd6901ed0960413ff3adefd5abfa80cc8b5a6f7
Keys <keys14052002@gmail.com>
2025-09-11 10:21:21 +0000

    refactor: cater for the UI changes in the options

--- options1.php
+++ options1.php
@@ -20,6 +20,7 @@
     $template = <<<XML
     <?xml version="1.0" encoding="UTF-8"?>
     <options>
+    <option>Select color scheme</option>
     XML;
 
     foreach ($options as $value) {
--- options2.php
+++ options2.php
@@ -5,6 +5,7 @@ $options = ['black', 'white', 'orange'];
 $template = <<<XML
 <?xml version="1.0" encoding="UTF-8"?>
 <options>
+<option>Select color scheme</option>
 XML;
 
 foreach ($options as $value) {
