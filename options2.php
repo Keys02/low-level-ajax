21219b5aa599057edbbac5c3f14aede5446978cb
Keys <keys14052002@gmail.com>
2025-09-11 08:49:51 +0000

    feat: load the second color scheme colors from the xml file

--- options2.php
+++ options2.php
@@ -3,11 +3,18 @@ header("Content-type: text/xml");
 $options = ['black', 'white', 'orange'];
 
 $template = <<<XML
-    <?xml version="1.0"?>;
+<?xml version="1.0" encoding="UTF-8"?>
+<options>
 XML;
 
 foreach ($options as $value) {
-    $template += <<<XML
-        <option>$value</option>;
+    $template .= <<<XML
+    <option>$value</option>
     XML;
 }
+
+$template .= <<<XML
+</options>
+XML;
+
+echo $template;
