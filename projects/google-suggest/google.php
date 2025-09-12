d27bc8798d667d283d1404248bedeea385f1b3fe
Keys <keys14052002@gmail.com>
2025-09-12 17:24:56 +0000

    feat: fetch data using the google suggest url

---
+++ projects/google-suggest/google.php
@@ -0,0 +1,12 @@
+<?php
+    $question = filter_input(INPUT_GET, "qu");
+    // $question = "chuck norris";
+
+    $handle = fopen("https://www.google.com/complete/search?q=" . urlencode($question) . "w&cp=1&client=gws-wiz&xssi=t&gs_pcrt=undefined&hl=en-GH&authuser=0&psi=ODXEaJLQFJnV1sQP5df_mAc.1757689145235&dpr=1.25&pq=How%20to%20paste%20from%20clipboard%20in%20vim", "r");
+
+    while (!feof($handle)) {
+        $text = fgets($handle);
+        echo $text;
+    }
+
+    fclose($handle);
